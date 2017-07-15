5a9626e965f1a19970876d2b9e690c952f8c1451
Tom Everett <tom@khubla.com>
2017-07-15 17:56:14 -0600

    first commit

---
+++ src/main/java/com/khubla/antlr4example/Main.java
@@ -0,0 +1,44 @@
+package com.khubla.antlr4example;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.TokenStream;
+
+import com.khubla.antlr4example.arithmeticParser.EquationContext;
+
+/**
+ * @author Tom Everett
+ */
+class Main {
+   public static void main(String[] args) {
+      System.out.println("Antlr4 Example");
+      try {
+         /*
+          * get the input file as an InputStream
+          */
+         InputStream inputStream = Main.class.getResourceAsStream("/pyth.txt");
+         /*
+          * make Lexer
+          */
+         Lexer lexer = new arithmeticLexer(CharStreams.fromStream(inputStream));
+         /*
+          * get a TokenStream on the Lexer
+          */
+         TokenStream tokenStream = new CommonTokenStream(lexer);
+         /*
+          * make a Parser on the token stream
+          */
+         arithmeticParser parser = new arithmeticParser(tokenStream);
+         /*
+          * get the top node of the AST. This corresponds to the topmost rule of equation.q4, "equation"
+          */
+         EquationContext equationContext = parser.equation();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+}
